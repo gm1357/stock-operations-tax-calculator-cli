65a98c3ca5a9b116edbae1d2070ec0900726e645
Gabriel <g_machado1@hotmail.com>
2025-09-12 11:47:16 -0300

    feat: add implementation that passes tests

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+export const TAX_FREE_THRESHOLD = 20000;
+export const TAX_RATE = 0.2;
+
+export const OPERATION_TYPE = {
+  BUY: 'buy',
+  SELL: 'sell',
+};
--- src/tax-calculator.js
+++ src/tax-calculator.js
@@ -1,9 +1,55 @@
+import { OPERATION_TYPE, TAX_FREE_THRESHOLD, TAX_RATE } from './constants.js';
+import { calculatePriceWeightedAverage } from './utils/capital-operations.js';
+
 export function calculateManyLedgersTaxes(operationsLedgers) {
   return operationsLedgers.map((ledger) => calculateLedgerTaxes(ledger));
 }
 
 function calculateLedgerTaxes(operationsLedger) {
-  return operationsLedger.map(() => {
-    return { tax: 0 };
-  });
+  const taxes = [];
+  let weightedAveragePrice = 0;
+  let stockQuantity = 0;
+  let accumulatedLoss = 0;
+
+  for (const operation of operationsLedger) {
+    const {
+      operation: operationType,
+      quantity,
+      'unit-cost': unitCost,
+    } = operation;
+    // Buy operation
+    if (operationType === OPERATION_TYPE.BUY) {
+      weightedAveragePrice = calculatePriceWeightedAverage({
+        currentStockQuantity: stockQuantity,
+        previousWeightedAverage: weightedAveragePrice,
+        boughtQuantity: quantity,
+        currentUnitCost: unitCost,
+      });
+      stockQuantity += quantity;
+      taxes.push(0);
+      continue;
+    }
+
+    // Sell operation
+    stockQuantity -= quantity;
+
+    if (unitCost <= weightedAveragePrice) {
+      const loss = (weightedAveragePrice - unitCost) * quantity;
+      accumulatedLoss += loss;
+      taxes.push(0);
+      continue;
+    }
+
+    if (unitCost * quantity <= TAX_FREE_THRESHOLD) {
+      taxes.push(0);
+      continue;
+    }
+
+    const profit = (unitCost - weightedAveragePrice) * quantity;
+    const profitAfterLossCompensation = Math.max(0, profit - accumulatedLoss);
+    accumulatedLoss = Math.max(0, accumulatedLoss - profit);
+    taxes.push(profitAfterLossCompensation * TAX_RATE);
+  }
+
+  return taxes.map((tax) => ({ tax }));
 }
